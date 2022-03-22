f149a5dfc9e75bb58f730bf422caad9de350e945
CPA-Developer <subinsebastian017@gmail.com>
2022-03-22 14:59:04 -0400

    added parcel and babel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 // /* global React */
 // /* global ReactDOM */ to remove the syntax error caused by eslint
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet.js"
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
