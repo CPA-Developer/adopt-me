ce7ac054f81117732abe65e9aebc2ed966e1a2dd
CPA-Developer <subinsebastian017@gmail.com>
2022-03-22 15:54:57 -0400

    added hooks and eslint recommendation for it

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
+
 const SearchParams = () => {
-    const location = "Seattle, WA";
+
+   
+    // const locationTuple =useState("Seattle, WA");
+    // const location = locationTuple[0];
+    // const setLocation = locationTuple[1];
+    const [location, setLocation]=useState("Seattle, WA");
+    function updateLocation(e){
+        setLocation(e.target.value);
+    }
+    //const location = "Seattle, WA";
     return (
       <div className="search-params">
         <form>
           <label htmlFor="location">
             Location
-            <input id="location" value={location} placeholder="Location" />
+            <input id="location" 
+            // onChange={e => setLocation(e.target.value)}
+            onChange={updateLocation}
+             value={location} placeholder="Location" />
           </label>
           <button>Submit</button>
         </form>
